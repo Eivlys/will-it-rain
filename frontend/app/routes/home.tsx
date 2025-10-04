578e228f054297729f847fec016b87caa405e0e8
Eivlys <vamcheryl@gmail.com>
2025-10-04 17:53:11 -0400

    add test weather map

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,4 +1,4 @@
-import { APIProvider, ControlPosition, Map } from '@vis.gl/react-google-maps';
+import { APIProvider, Map } from '@vis.gl/react-google-maps';
 
 // @ts-ignore
 const API_KEY: string = "AIzaSyCHuvlAD3QkDbJXRF4OSy_SM75gLMLUka8" as string;
@@ -8,7 +8,7 @@ const Home = () => {
 
   return (
     <>
-      <APIProvider apiKey={API_KEY}>
+      {/* <APIProvider apiKey={API_KEY}>
         <Map
           style={{ width: '100vw', height: '100vh' }}
           defaultCenter={{ lat: 22.54992, lng: 0 }}
@@ -16,7 +16,12 @@ const Home = () => {
           gestureHandling='greedy'
           disableDefaultUI
         />
-      </APIProvider>
+      </APIProvider> */}
+      <iframe
+        title="Example Website"
+        width="100%"
+        height="500px"
+        src="https://api.meteomatics.com/2025-07-20T17Z/precip_5min:mm,hail_5min:cm/47.6,7.2_47.2,8:0.001,0.001/html_map"></iframe>
     </>
   );
 };
