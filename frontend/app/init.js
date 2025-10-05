1dc3577e6a81f18c1e7aa37238074a3355b4039a
Eivlys <vamcheryl@gmail.com>
2025-10-05 12:21:04 -0400

    change home page and aadd line charts

---
+++ frontend/app/init.js
@@ -0,0 +1,32 @@
+
+
+export function initGoogle() {
+    (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={ });var d=b.maps||(b.maps={ }),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await(a = m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+    ({ key: "AIzaSyBrNnw4HusfjQezVaLSctpSOGtgTCYU5RM", v: "weekly" });
+}
+
+export async function initMap() {
+    // Request needed libraries.
+    await google.maps.importLibrary("places");
+    // Create the input HTML element, and append it.
+    //@ts-ignore
+    const placeAutocomplete = new google.maps.places.PlaceAutocompleteElement();
+    //@ts-ignore
+    const searchContainer = document.querySelector(".search-container");
+    searchContainer.appendChild(placeAutocomplete);
+    // Inject HTML UI.
+    const selectedPlaceTitle = document.createElement('p');
+    selectedPlaceTitle.textContent = '';
+    document.body.appendChild(selectedPlaceTitle);
+    const selectedPlaceInfo = document.createElement('pre');
+    selectedPlaceInfo.textContent = '';
+    document.body.appendChild(selectedPlaceInfo);
+    // Add the gmp-placeselect listener, and display the results.
+    //@ts-ignore
+    placeAutocomplete.addEventListener('gmp-select', async ({ placePrediction }) => {
+        const place = placePrediction.toPlace();
+        await place.fetchFields({ fields: ['displayName', 'formattedAddress', 'location'] });
+        // selectedPlaceTitle.textContent = 'Selected Place:';
+        // selectedPlaceInfo.textContent = JSON.stringify(place.toJSON(), /* replacer */ null, /* space */ 2);
+    });
+}
